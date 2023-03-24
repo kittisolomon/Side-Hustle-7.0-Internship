bc7badb4ec131159ece3191727b97173eb1f6c48
Kitti Solomon <40053238+kittisolomon@users.noreply.github.com>
2023-03-24 18:30:14 +0100

    Update week2task.php

--- week2task.php
+++ week2task.php
@@ -6,7 +6,7 @@ $voterWard = 020;
 
 if($voterAge < 18){
     echo "Voter is not old enough to vote.";
-}elseif($voterHasPVC === false){
+}elseif($voterHasPVC !== true){
     echo "Sorry you can not vote without a PVC.";
 }elseif($voterWard !== 020){
     echo "Oops this is not your ward";
